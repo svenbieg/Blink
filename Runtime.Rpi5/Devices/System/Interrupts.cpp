c0ac86671b458a3081fb4dca1939cb3d3566e2f6
Sven Bieg <svenbieg@outlook.de>
2025-03-22 11:03:05 +0100

    Mutex throws in ISR

--- Runtime.Rpi5/Devices/System/Interrupts.cpp
+++ Runtime.Rpi5/Devices/System/Interrupts.cpp
@@ -76,7 +76,7 @@ constexpr UINT GICC_IAR_IRQ_MASK=0x3FF;
 // Handler
 //=========
 
-extern "C" VOID HandleInterrupt()noexcept
+extern "C" VOID HandleInterrupt()
 {
 auto gicc=(GICC_REGS*)ARM_GICC_BASE;
 UINT iar=BitHelper::Get(gicc->IAR);
@@ -92,14 +92,20 @@ BitHelper::Write(gicc->EOIR, iar);
 // Common
 //========
 
-VOID Interrupts::Disable()noexcept
+BOOL Interrupts::Active()
+{
+UINT core=Cpu::GetId();
+return s_Active[core];
+}
+
+VOID Interrupts::Disable()
 {
 Cpu::DisableInterrupts();
 UINT core=Cpu::GetId();
 s_DisableCount[core]++;
 }
 
-VOID Interrupts::Disable(UINT irq)noexcept
+VOID Interrupts::Disable(UINT irq)
 {
 assert(irq<IRQ_COUNT);
 auto gicd=(GICD_REGS*)ARM_GICD_BASE;
@@ -108,14 +114,14 @@ UINT mask=1<<(irq%32);
 BitHelper::Set(gicd->CLEAR_ENABLED[reg], mask);
 }
 
-VOID Interrupts::Enable()noexcept
+VOID Interrupts::Enable()
 {
 UINT core=Cpu::GetId();
 if(--s_DisableCount[core]==0)
 	Cpu::EnableInterrupts();
 }
 
-VOID Interrupts::Enable(UINT irq)noexcept
+VOID Interrupts::Enable(UINT irq)
 {
 assert(irq<IRQ_COUNT);
 auto gicd=(GICD_REGS*)ARM_GICD_BASE;
@@ -124,16 +130,24 @@ UINT mask=1<<(irq%32);
 BitHelper::Set(gicd->SET_ENABLED[reg], mask);
 }
 
-VOID Interrupts::HandleInterrupt(UINT irq)noexcept
+BOOL Interrupts::Enabled()
 {
 UINT core=Cpu::GetId();
+return s_DisableCount[core]==0;
+}
+
+VOID Interrupts::HandleInterrupt(UINT irq)
+{
+UINT core=Cpu::GetId();
+s_Active[core]=true;
 s_DisableCount[core]++;
 if(s_IrqHandler[irq])
 	s_IrqHandler[irq](s_IrqParameter[irq]);
+s_Active[core]=false;
 s_DisableCount[core]--;
 }
 
-VOID Interrupts::Initialize()noexcept
+VOID Interrupts::Initialize()
 {
 auto gicd=(GICD_REGS*)ARM_GICD_BASE;
 BitHelper::Write(gicd->CTRL, GICD_CTRL_DISABLE);
@@ -160,7 +174,7 @@ for(UINT core=0; core<CPU_COUNT; core++)
 	s_DisableCount[core]=1;
 }
 
-VOID Interrupts::Route(UINT irq, IrqTarget target)noexcept
+VOID Interrupts::Route(UINT irq, IrqTarget target)
 {
 auto gicd=(GICD_REGS*)ARM_GICD_BASE;
 UINT reg=irq/4;
@@ -170,7 +184,7 @@ UINT value=(UINT)target<<id;
 BitHelper::Set(gicd->TARGET[reg], mask, value);
 }
 
-VOID Interrupts::Send(UINT irq, IrqTarget target)noexcept
+VOID Interrupts::Send(UINT irq, IrqTarget target)
 {
 auto gicd=(GICD_REGS*)ARM_GICD_BASE;
 UINT value=0;
@@ -179,7 +193,7 @@ BitHelper::Set(value, irq);
 BitHelper::Write(gicd->SGIR, value);
 }
 
-VOID Interrupts::SetHandler(UINT irq, IRQ_HANDLER handler, VOID* param)noexcept
+VOID Interrupts::SetHandler(UINT irq, IRQ_HANDLER handler, VOID* param)
 {
 assert(irq<IRQ_COUNT);
 Disable(irq);
@@ -194,6 +208,7 @@ if(handler)
 // Common Private
 //================
 
+BOOL Interrupts::s_Active[CPU_COUNT]={ false };
 UINT Interrupts::s_DisableCount[CPU_COUNT]={ 0 };
 IRQ_HANDLER Interrupts::s_IrqHandler[IRQ_COUNT]={ nullptr };
 VOID* Interrupts::s_IrqParameter[IRQ_COUNT]={ nullptr };
--- Runtime.Rpi5/Devices/System/Interrupts.h
+++ Runtime.Rpi5/Devices/System/Interrupts.h
@@ -51,19 +51,22 @@ class Interrupts
 {
 public:
 	// Common
-	static VOID Disable()noexcept;
-	static VOID Disable(UINT Irq)noexcept;
-	static VOID Enable()noexcept;
-	static VOID Enable(UINT Irq)noexcept;
-	static VOID HandleInterrupt(UINT Irq)noexcept;
-	static VOID Initialize()noexcept;
-	static VOID Route(UINT Irq, IrqTarget Target)noexcept;
-	static inline VOID Send(UINT Irq, UINT Core)noexcept { Send(Irq, (IrqTarget)(1<<Core)); }
-	static VOID Send(UINT Irq, IrqTarget Target)noexcept;
-	static VOID SetHandler(UINT Irq, IRQ_HANDLER Handler, VOID* Parameter=nullptr)noexcept;
+	static BOOL Active();
+	static VOID Disable();
+	static VOID Disable(UINT Irq);
+	static VOID Enable();
+	static VOID Enable(UINT Irq);
+	static BOOL Enabled();
+	static VOID HandleInterrupt(UINT Irq);
+	static VOID Initialize();
+	static VOID Route(UINT Irq, IrqTarget Target);
+	static inline VOID Send(UINT Irq, UINT Core) { Send(Irq, (IrqTarget)(1<<Core)); }
+	static VOID Send(UINT Irq, IrqTarget Target);
+	static VOID SetHandler(UINT Irq, IRQ_HANDLER Handler, VOID* Parameter=nullptr);
 
 private:
 	// Common
+	static BOOL s_Active[CPU_COUNT];
 	static UINT s_DisableCount[CPU_COUNT];
 	static IRQ_HANDLER s_IrqHandler[IRQ_COUNT];
 	static VOID* s_IrqParameter[IRQ_COUNT];
