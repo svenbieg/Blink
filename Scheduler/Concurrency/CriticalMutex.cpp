84789b2a25b86b26dfa850058da771240471a066
Sven Bieg <svenbieg@outlook.de>
2025-12-10 18:19:49 +0100

    CriticalMutex

--- Scheduler/Concurrency/CriticalMutex.cpp
+++ Scheduler/Concurrency/CriticalMutex.cpp
@@ -37,15 +37,17 @@ auto current=Scheduler::s_CurrentTask[core];
 if(!current)
 	return;
 assert(!FlagHelper::Get(current->m_Flags, TaskFlags::Sharing));
-FlagHelper::Set(current->m_Flags, TaskFlags::Locked);
-current->m_LockCount++;
 if(!m_Owner)
 	{
+	FlagHelper::Set(current->m_Flags, TaskFlags::Locked);
+	current->m_LockCount++;
 	m_Owner=current;
 	return;
 	}
 AddWaitingTask(current);
 Scheduler::SuspendCurrentTask(core, current);
+FlagHelper::Set(current->m_Flags, TaskFlags::Locked);
+current->m_LockCount++;
 }
 
 VOID CriticalMutex::Lock(AccessMode)
@@ -55,20 +57,24 @@ SpinLock lock(Scheduler::s_CriticalSection);
 UINT core=Cpu::GetId();
 auto current=Scheduler::s_CurrentTask[core];
 assert(!FlagHelper::Get(current->m_Flags, TaskFlags::Sharing));
-FlagHelper::Set(current->m_Flags, TaskFlags::LockedSharing);
-current->m_LockCount++;
 if(!m_Owner)
 	{
+	FlagHelper::Set(current->m_Flags, TaskFlags::LockedSharing);
+	current->m_LockCount++;
 	m_Owner=current;
 	return;
 	}
 if(FlagHelper::Get(m_Owner->m_Flags, TaskFlags::Sharing))
 	{
+	FlagHelper::Set(current->m_Flags, TaskFlags::LockedSharing);
+	current->m_LockCount++;
 	Scheduler::AddParallelTask(&m_Owner, current);
 	return;
 	}
 AddWaitingTask(current, AccessMode::ReadOnly);
 Scheduler::SuspendCurrentTask(core, current);
+FlagHelper::Set(current->m_Flags, TaskFlags::LockedSharing);
+current->m_LockCount++;
 }
 
 BOOL CriticalMutex::TryLock()
