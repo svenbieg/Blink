608451bba99e12dfea590c06f4ee7f4cb19a4812
Sven Bieg <svenbieg@outlook.de>
2026-01-05 04:03:29 +0100

    VariableArguments

--- Runtime/attr.h
+++ Runtime/attr.h
@@ -10,8 +10,6 @@
 //============
 
 #define __align(x) __attribute__((aligned(x)))
-#define __always_inline __attribute__((always_inline)) inline
 #define __no_data(align) __attribute__((aligned(align))) __attribute__((section(".nodata")))
 #define __no_return __attribute__((noreturn))
-#define __packed __attribute__((packed))
 #define __section(x) __attribute__((section(x)))
