9ebe88c11fea472365e7417be8fbf57cda6b7259
Sven Bieg <svenbieg@outlook.de>
2026-03-21 12:12:04 +0100

    Moved TaskHelper

--- Runtime.Rpi5/Concurrency/TaskHelper.cpp
+++ Runtime.Rpi5/Concurrency/TaskHelper.cpp
@@ -31,26 +31,23 @@ namespace Concurrency {
 
 VOID TaskHelper::Initialize(SIZE_T* StackPointer, VOID (*TaskProc)(VOID*), VOID* Parameter)noexcept
 {
-task_init(StackPointer, TaskProc, Parameter);
-}
-
-SIZE_T TaskHelper::RestoreContext(SIZE_T StackPointer)noexcept
-{
-return task_restore_context(StackPointer);
-}
-
-SIZE_T TaskHelper::SaveContext(SIZE_T StackPointer)noexcept
-{
-return task_save_context(StackPointer);
+SIZE_T stack=*StackPointer;
+stack-=sizeof(IRQ_FRAME);
+auto irq_frame=(IRQ_FRAME*)stack;
+irq_frame->X0=(SIZE_T)Parameter;
+stack-=sizeof(TASK_FRAME);
+auto task_frame=(TASK_FRAME*)stack;
+task_frame->ELR=(SIZE_T)TaskProc;
+*StackPointer=stack;
 }
 
 VOID TaskHelper::Switch(UINT core, Task* current, Task* next)noexcept
 {
 SIZE_T stack_end=(SIZE_T)&__stack_end;
 auto irq_stack=(IRQ_STACK*)(stack_end-core*CONFIG_STACK_SIZE-sizeof(IRQ_STACK));
-assert(irq_stack->SP-sizeof(TASK_FRAME)>=current->m_StackBottom); // Stack-overflow
-current->m_StackPointer=SaveContext(irq_stack->SP);
-irq_stack->SP=RestoreContext(next->m_StackPointer);
+assert(irq_stack->SP-sizeof(TASK_FRAME)>=current->m_StackBottom); // Stack-Overflow
+current->m_StackPointer=task_save_context(irq_stack->SP);
+irq_stack->SP=task_restore_context(next->m_StackPointer);
 }
 
 }
--- Runtime.Rpi5/Concurrency/TaskHelper.h
+++ Runtime.Rpi5/Concurrency/TaskHelper.h
@@ -35,8 +35,6 @@ class TaskHelper
 public:
 	// Common
 	static VOID Initialize(SIZE_T* StackPointer, VOID (*TaskProc)(VOID*), VOID* Parameter)noexcept;
-	static SIZE_T RestoreContext(SIZE_T StackPointer)noexcept;
-	static SIZE_T SaveContext(SIZE_T StackPointer)noexcept;
 	static VOID Switch(UINT Core, Task* Current, Task* Next)noexcept;
 };
 
