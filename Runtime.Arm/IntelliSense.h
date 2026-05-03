bc3ad8a6a55063d0a346d6e23ac8903115132f4e
Sven Bieg <svenbieg@outlook.de>
2026-05-03 16:52:57 +0200

    Compiler version

--- Runtime.Arm/IntelliSense.h
+++ Runtime.Arm/IntelliSense.h
@@ -9,7 +9,7 @@
 // Forward-Declarations
 //======================
 
-#define __GNUC__ 14
+#define __GNUC__ 15
 #define __GNUC_MINOR__ 2
 
 #define __PTRDIFF_TYPE__ int
@@ -19,16 +19,6 @@
 #define __ORDER_LITTLE_ENDIAN__ 0
 #define __ORDER_BIG_ENDIAN__ 1
 #define __BYTE_ORDER__ 2
-
-#define __builtin_labs(l) 0
-#define __builtin_llabs(l) 0
-
-
-//========
-// Limits
-//========
-
-#define __LONG_LONG_MAX__ 0x7FFFFFFFFFFFFFFFULL
 
 
 //=======
